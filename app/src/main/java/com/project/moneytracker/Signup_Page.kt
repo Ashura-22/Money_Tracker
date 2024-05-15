a57f0abb84de2a34ee4dcfed9f6c0df72f002d22
Ashura-22 <Rishikeshjadhav0.rj@gmail.com>
2024-05-15 16:46:19 +0530

    project intial setup on git

--- app/src/main/java/com/project/moneytracker/Signup_Page.kt
+++ app/src/main/java/com/project/moneytracker/Signup_Page.kt
@@ -2,11 +2,13 @@ package com.project.moneytracker
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
 import com.project.moneytracker.databinding.ActivitySignupPageBinding
 
@@ -14,7 +16,52 @@ class Signup_Page : AppCompatActivity(){
 
     private lateinit var binding: ActivitySignupPageBinding
     private lateinit var firebaseAuth: FirebaseAuth
-    val db = Firebase.firestore
+
+    private lateinit var db: FirebaseFirestore
+
+//Both signIn and saveUserDetails are used to retrieve uid from auth table and map it to user details table in firestore
+    //to keep user credentials secure and separate from user_data table
+    //--------------------------Start--------------------------------
+    private fun signIn(name: String,email: String, password: String) {
+
+        val auth = FirebaseAuth.getInstance()
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, retrieve the user UID
+                    val user = auth.currentUser
+                    user?.let {
+                        val uid = it.uid
+                        // Now map this UID to your custom user details
+                        saveUserDetails(name,uid)
+                    }
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("SignIn", "signInWithEmail:failure", task.exception)
+                }
+            }
+    auth.signOut()  //to terminate session
+    }
+
+    private fun saveUserDetails(name: String,uid: String) {
+
+        //Log.d("Firestore", "Saving user details for UID: $uid")
+        val userDetails = hashMapOf(
+            "name" to name)
+
+        db.collection("user_data").document(uid)
+            .set(userDetails)
+            .addOnSuccessListener {
+                Log.d("Firestore", "DocumentSnapshot successfully written!")
+                //Toast.makeText( this,"Successfully Saved", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+                Log.w("Firestore", "Error writing document", e)
+            }
+    }
+
+//--------------------------End--------------------------------
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,20 +110,13 @@ class Signup_Page : AppCompatActivity(){
                             //By mapping name with uid
                             //user logged in because to retrieve uid using firebaseAuth via mail
                             //and signed out after successful retrieving uid
-                            firebaseAuth.signInWithEmailAndPassword(mail, pwd).addOnCompleteListener {
-                                //this will store name in firebase
-                                val ud = FirebaseAuth.getInstance().uid
-                                    /*
-                                    code not working
-                                    if (ud != null) {
-                                        db.collection("user_data").document(ud).set(name)
-                                    }*/
-                                binding.textView6.text=ud
-                                firebaseAuth.signOut()
-                            }
+
+                            db = FirebaseFirestore.getInstance()
+                            signIn(name,mail,pwd)
+
                             //redirecting to login page
-                            //val intent= Intent(this,Login_Page::class.java)
-                            //startActivity(intent)
+                            val intent= Intent(this,Login_Page::class.java)
+                            startActivity(intent)
                         }
                         else
                         {
